fcc2e29b315a54b641b4b93408b78ec234163a9e
Atsushi Kokubo <kokubo@hi-tech.ac.jp>
2022-01-14 16:31:12 +0900

    モデルの修正

--- app/models/genre.rb
+++ app/models/genre.rb
@@ -1,2 +1,4 @@
 class Genre < ApplicationRecord
+  has_many :recipe_genres
+  has_many :recipes, through: :recipe_genres, dependent: :destroy
 end
--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -1,2 +1,4 @@
 class Recipe < ApplicationRecord
+  has_many :recipe_genres
+  has_many :genres, through: :recipe_genres, dependent: :destroy
 end
