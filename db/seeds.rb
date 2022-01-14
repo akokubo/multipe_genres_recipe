9caf09076b7e54486e2ed5d4623e4c2735c33233
Atsushi Kokubo <kokubo@hi-tech.ac.jp>
2022-01-14 16:33:28 +0900

    シードの作成

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,24 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Genre.create([
+  {name: '和食'},
+  {name: '洋食'},
+  {name: '中華'}
+])
+
+Recipe.create([
+  {name: '味噌汁'},
+  {name: 'ハンバーグ'},
+  {name: '麻婆豆腐'},
+  {name: 'トンカツ'}
+])
+
+RecipeGenre.create([
+  {recipe: Recipe.find_by(name: '味噌汁'), genre: Genre.find_by(name: '和食')},
+  {recipe: Recipe.find_by(name: 'ハンバーグ'), genre: Genre.find_by(name: '洋食')},
+  {recipe: Recipe.find_by(name: '麻婆豆腐'), genre: Genre.find_by(name: '中華')},
+  {recipe: Recipe.find_by(name: 'トンカツ'), genre: Genre.find_by(name: '和食')},
+  {recipe: Recipe.find_by(name: 'トンカツ'), genre: Genre.find_by(name: '洋食')}
+])
